ced17d49a52cfdb4c6786c79b88acb84bd4fadab
Taylor <bKuzzcs@gmail.com>
2022-03-03 13:54:25 -0700

    commit tree 92fae07cc904231ecc1ff0c028a1a354cf5460d2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement=document.querySelector("#main").remove();
+
+const newHeader=document.createElement('h1');
+newHeader.id="victory";
+newHeader.setAttribute="h1#victory";
+newHeader.innerHTML="Taylor is the champion";
